ab547064c1da65e1b6f47a093f94c8759cf7d2d4
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-05-30 09:15:49 -0400

    Update
    Project config
    
    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Footer from "./components/FooterDiv/Footer";
+import Job from "./components/JobDiv/Job";
+import NavBar from "./components/NavBarDiv/NavBar";
+import Search from "./components/SearchDiv/Search";
+import Value from "./components/ValueDiv/Value";
+
 const App = () => {
-  return <> Test</>;
+  return (
+    <div className="w-[85%] m-auto bg-white">
+      <NavBar />
+      <Search />
+      <Job/>
+      <Value/>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
